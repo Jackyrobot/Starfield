74e63dbbddc0df5fdce85fc63099121ca23a1ec9
unknown <mili7@697-334-Smaug.sf.k12.internal>
2014-10-21 11:03:46 -0700

    Added normal particles, oddball particles, and particle interface.

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -1,5 +1,21 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 Particle[] particles;
-void setup()
+public void setup()
 {
 	size(600, 600);
 	background(22, 23, 36);
@@ -21,7 +37,7 @@ void setup()
 	}
 }
 int offScreen = 0;
-void draw()
+public void draw()
 {
 	background(22, 23, 36);
 	for(int i = 0; i < particles.length; i++)
@@ -101,3 +117,12 @@ class OddballParticle implements Particle
 }
 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
